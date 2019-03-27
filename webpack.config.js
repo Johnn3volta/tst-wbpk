26fa633adce67aeab110cb9437dc5a64491ca297
Mikhail Tsalka <mikhail.tsalka@1gx.org>
2019-03-27 12:50:10 +0300

    add comments to webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,47 @@
-const path = require('path'),
-  ProgressBarPlugin = require('progress-bar-webpack-plugin'),
-  CopyWebpackPlugin = require('copy-webpack-plugin'),
-  HtmlWebpackPlugin = require('html-webpack-plugin'),
-  CleanWebpackPlugin = require('clean-webpack-plugin'),
-  webpack = require('webpack'),
-  VueLoaderPlugin = require('vue-loader/lib/plugin'),
+const path             = require('path'),
+  ProgressBarPlugin    = require('progress-bar-webpack-plugin'),
+  CopyWebpackPlugin    = require('copy-webpack-plugin'),
+  HtmlWebpackPlugin    = require('html-webpack-plugin'),
+  CleanWebpackPlugin   = require('clean-webpack-plugin'),
+  VueLoaderPlugin      = require('vue-loader/lib/plugin'),
   MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 
 module.exports = (env) => {
+  /* присваиваем зависимость dev в зависимости от запущенной клманды. Проставляется в package.json scripts --env  */
   let devMode = env === 'dev' ? env : false;
 
   let conf = {
-
+    // Точки входа
     entry: {
+      // Имя точки входа
       main: [
+        // файл js для данной точки входа
         './src/js/main.js',
+        // файл scss для данной точки входа
         './src/styles/style.scss'
       ],
     },
+    // Точка выхода для js
     output: {
+      // Имя выходного js файла. [name] - подставляется имя тчоки входа
       filename: 'js/[name].js',
+      // Путь куда компилируются файлы
       path: path.resolve(__dirname, 'build')
     },
+    // Отвечает за sourceMaps
     devtool: devMode ? 'source-map' : false,
+    // Настройки devServera
     devServer: {
+      // При запуске открывает сразу в браузере
       open: true,
+      // Показывает ошибки в окне браузера
       overlay: true
     },
     module: {
+      // правила обработки файлов
       rules: [
+        // отвечает за файлы vue
         {
           test: /\.vue$/,
           loader: 'vue-loader',
@@ -39,6 +51,7 @@ module.exports = (env) => {
             }
           }
         },
+        // отвечает за файлы css,scss,sass
         {
           test: /\.(sa|sc|c)ss$/,
           use: [
@@ -48,6 +61,7 @@ module.exports = (env) => {
             'sass-loader?sourceMap'
           ],
         },
+        // отвечает за файлы styl
         {
           test: /\.styl$/,
           loader: [
@@ -56,12 +70,16 @@ module.exports = (env) => {
             'postcss-loader',
             'stylus-loader'],
         },
+        // отвечает за файлы изображений.
         {
           test: /\.(jpe?g|png|gif|svg)$/,
           use: {
+            // подгружает файлы картинок которые прописаны в стилях
             loader: 'url-loader',
             options: {
+              // если размер файла меньше указаного лимита картинка из стилей подключается в base64
               limit: 8000,
+              // меняет слешы на линуксовские
               name(url) {
                 const destination = path.relative(path.resolve(__dirname, 'src'), url);
 
@@ -72,12 +90,14 @@ module.exports = (env) => {
             }
           }
         },
+        // отвечает за файлы шрифтов
         {
           test: /\.(woff|woff2|eot|ttf|otf)$/,
           use: [
             {
               loader: 'file-loader',
               options: {
+                // меняет слешы на линуксовские
                 name(url) {
                   const destination = path.relative(path.resolve(__dirname, 'src'), url);
 
@@ -98,12 +118,15 @@ module.exports = (env) => {
     },
 
     plugins: [
+      // Показывет в консоли прогресс компиляции
       new ProgressBarPlugin(),
+      // Компиляция css
       new MiniCssExtractPlugin(
         {
           filename: 'css/[name].min.css'
         }
       ),
+      // Копирование файлов
       new CopyWebpackPlugin([
         {
           from: './src/images',
@@ -111,20 +134,24 @@ module.exports = (env) => {
         },
       ]),
       new VueLoaderPlugin(),
+      // Компиляция html
       new HtmlWebpackPlugin({
+        // исходный файл
         template: './src/index.html',
+        // Название компилируемого файла
         filename: 'index.html',
+        // Название точки входа для подтягивания css и js. С свйойством inject: false скрипты и стили подключаем вручну в исходном файле
         chunks: [
           'main'
         ],
       }),
     ],
   };
-
+  // Очистка папки buid перед компиляцией
   if (!devMode) {
     conf.plugins.push(new CleanWebpackPlugin())
   }
-
+  // Компиляция с babel или без, в зависимости от дерективы --env
   if (env === 'babel') {
     conf.module.rules.push(
       {
